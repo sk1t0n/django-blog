ba32f3cbe1bf21a3d64d691821939a341aa2646a
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-03-01 18:41:34 +0300

    Add views: PostList, PostDetail

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Post
+
+
+class PostListView(ListView):
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.filter(is_published=True)
+    paginate_by = 5
+
+
+class PostDetailView(DetailView):
+    template_name = 'blog/post_detail.html'
+
+    def get_queryset(self):
+        slug = self.kwargs['slug']
+        return Post.objects.filter(is_published=True, slug=slug)

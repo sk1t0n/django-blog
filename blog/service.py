c2341e0fac534230e1678e5bd7c05f1dd38bb322
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-03-23 11:59:58 +0300

    Add search posts

--- blog/service.py
+++ blog/service.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from django.db.models import Q
+from django.utils import timezone
+
 from .config import LIMIT_RECENT_POSTS
 from .models import Post, Tag
 
@@ -8,3 +12,31 @@ def get_recent_posts(limit: int = LIMIT_RECENT_POSTS):
 
 def get_all_tags():
     return Tag.objects.all()
+
+
+def get_posts(search_query):
+    posts = Post.objects.filter(
+        is_published=True,
+        published_date__lte=timezone.localtime()
+    )
+    if search_query:
+        data_search = search_query.strip().split(' ')
+        if len(data_search) == 1:
+            tag = Tag.objects.filter(name__icontains=data_search[0]).first()
+            # search by tag
+            if tag:
+                posts = posts.filter(tags__in=[tag.pk])
+            # search by title or content
+            else:
+                posts = posts.filter(
+                    Q(title__icontains=data_search[0]) |
+                    Q(content__icontains=data_search[0])
+                ).distinct()
+        # search by title or content
+        else:
+            search_query = ' '.join([item for item in data_search if item != ''])  # noqa
+            posts = posts.filter(
+                Q(title__icontains=search_query) |
+                Q(content__icontains=search_query)
+            ).distinct()
+    return posts
--- blog/views/posts.py
+++ blog/views/posts.py
@@ -2,14 +2,18 @@ from django.views.generic import ListView, DetailView
 
 from ..config import POSTS_PER_PAGE
 from ..models import Post
+from ..service import get_posts
 from .utils import FillContextMixin
 
 
 class PostListView(FillContextMixin, ListView):
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    queryset = Post.objects.filter(is_published=True)
     paginate_by = POSTS_PER_PAGE
+
+    def get_queryset(self):
+        search_query = self.request.GET.get('search')
+        return get_posts(search_query)
 
 
 class PostDetailView(FillContextMixin, DetailView):
